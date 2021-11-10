b95f5d8f8387562f4da6c692071c8b36bd3415f4
Kustov Ilya <kusgent@yandex.ru>
2021-11-10 21:45:30 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,13 @@
 # https://stepik.org/lesson/199980/step/6?unit=174035
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
