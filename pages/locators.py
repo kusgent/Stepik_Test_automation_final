40c77abc3862e5cdc273e3defe4adc3091a8a88d
Kustov Ilya <kusgent@yandex.ru>
2021-11-21 14:44:57 +0300

    Add test test_guest_can_add_product_to_basket

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6 > h1")
+    MESSAGE_ABOUT_ADDITION = (By.CSS_SELECTOR, "#messages > .alert:nth-child(1) strong")
+    PRODUCT_COST = (By.CSS_SELECTOR, ".col-sm-6 > .price_color")
+    MESSAGE_WITH_BASKET_COST = (By.CSS_SELECTOR, "#messages > .alert:nth-child(3) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_link.click()
+
+    def should_be_message_about_add_to_basket(self):
+        assert self.is_element_present(
+            *ProductPageLocators.MESSAGE_ABOUT_ADDITION), "Message about addition product is not presented"
+        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDITION).text
+        assert message == name, "Added another product"
+
+    def should_be_message_with_basket_cost(self):
+        assert self.is_element_present(
+            *ProductPageLocators.MESSAGE_WITH_BASKET_COST), "Message with basket cost is not presented"
+        cost = self.browser.find_element(*ProductPageLocators.PRODUCT_COST).text
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_WITH_BASKET_COST).text
+        assert message == cost, "Basket cost is calculated incorrectly"
